bd2ccf86b6f76d238952fbfa70ca3ff1a0e97e44
Lea Mankowski <lea17mk@gmail.com>
2023-05-07 21:44:58 -0300

    cambios de estilo menores- cambio en la ruta (modificacion de path en settings)

--- oh_my_dog/settings.py
+++ oh_my_dog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #STATIC_URL = 'static/'
-STATIC_URL =  os.path.join(BASE_DIR, "staticfiles/")
+STATIC_URL =  os.path.join(BASE_DIR, "staticfiles") + os.path.sep
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
