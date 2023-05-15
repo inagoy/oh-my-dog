a3a5bb128cdb43428983c20f0b5e5860eb9de376
Lea Mankowski <lea17mk@gmail.com>
2023-05-14 23:38:42 -0300

    registrar usuario: envio de mail con contraseña aleatoria + botones desaparecen de acuerdo a estado de logeo

--- usuarios_y_perros/helpers.py
+++ usuarios_y_perros/helpers.py
@@ -1,6 +1,9 @@
 #funciones auxiliares
 
 from datetime import date
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils.crypto import get_random_string
 
 def es_menor_18(birth):
     now = date.today()
@@ -12,5 +15,14 @@ def es_menor_18(birth):
         )
     )
 
-def generarContraseña():
-    return 1234
+def generar_contraseña():
+    return get_random_string(8)
+
+def enviar_mail_bienvenida(contraseña):
+    send_mail(
+                    "Bienvenidx a ¡Oh my dog!",
+                    "Para ingresar por primera vez al sitio, utilice la contraseña: "+ contraseña,
+                    settings.EMAIL_HOST_USER,
+                    ["alive.soluciones.software@gmail.com"],
+                    fail_silently=False,
+                )
