9bd252f286816757ea5456a44f712eafd163b8c2
Vero C <cao.veronica@gmail.com>
2023-05-21 15:35:04 -0300

    Contestar publicación de adopción: mensaje

--- publicaciones/helpers.py
+++ publicaciones/helpers.py
@@ -2,13 +2,14 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def enviar_mail_contestar_adopcion(mail, nombre, mensaje):
+    texto_mensaje = f'Te dejó este mensaje de presentación: {mensaje}' if mensaje  else ""
     send_mail(
-                    "Solicitud de adopción en ¡Oh my dog!",
-                    nombre + " quiere adoptar a tu perro " + "publicado en ¡Oh my dog!. "
-                    "Si estás interesadx, escribile a su mail " + mail + "." +
-                    "\nSi concretan la adopción recordá que podés cerrar la publicación para no recibir más respuestas."
-                    "\nSaludos,\nEquipo de ¡Oh my dog!",
-                    settings.EMAIL_HOST_USER,
-                    ["alive.soluciones.software@gmail.com"],
-                    fail_silently=False,
-                )
+        "Solicitud de adopción en ¡Oh my dog!",
+        f'{nombre} quiere adoptar a tu perro publicado en ¡Oh my dog!. {texto_mensaje}'
+        f'\nSi estás interesadx, escribile a su mail {mail}.'
+        f'\nSi concretan la adopción recordá que podés cerrar la publicación para no recibir más respuestas. '
+        f'\nSaludos,\nEquipo de ¡Oh my dog!',
+        settings.EMAIL_HOST_USER,
+        ["alive.soluciones.software@gmail.com"],
+        fail_silently=False,
+        )
