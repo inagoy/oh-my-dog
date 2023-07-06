8a08c7112559cc9c3b964220ee5f5f8c3897f6bb
unknown <lea17@gmail.com>
2023-07-05 21:43:27 -0300

    model de trabajador

--- listados/models.py
+++ listados/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Trabajador(models.Model):
+    nombre_y_apellido = models.CharField(max_length=30, unique=True)
+    
+    class Zona(models.TextChoices):
+        SAN_MARTIN = "San Martin" , "San Martin"
+        ITALIA = "Italia" , "Italia"
+        BELGRANO = "Belgrano" , "Belgrano"
+        PASO = "Paso" , "Paso"
+        ROCHA = "Rocha" , "Rocha"
+
+    zona = models.CharField(
+        max_length=10,
+        choices=Zona.choices,
+    )
+
+    class Tipo(models.TextChoices):
+        PASEADOR = "P" , "Paseador"
+        CUIDADOR = "C" , "Cuidador"
+
+    tipo = models.CharField(
+        max_length=1,
+        choices=Tipo.choices,
+    )
+
+    habilitado = models.BooleanField()
+
+    fecha_fin_deshabilitacion = models.DateField(
+        verbose_name="Deshabilitar hasta"
+    )
+
+    REQUIRED_FIELDS = ['zona','nombre_y_apellido',"tipo","habilitado"]
